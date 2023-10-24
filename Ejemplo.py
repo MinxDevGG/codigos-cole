cf58a82e96c78b21bd6e9542e3bd1c68238d5365
MinxDevGG <148901365+MinxDevGG@users.noreply.github.com>
2023-10-24 12:55:43 -0500

    Add files via upload

---
+++ Ejemplo.py
@@ -0,0 +1,17 @@
+class perro():
+
+  def __init__(self, color, forma, tamaño):
+
+    self.color = color,
+
+    self.forma = forma,
+
+    self.tamaño = tamaño
+
+  def ladrar():
+
+    print("guau")
+
+pastor_aleman = perro("negro", "perro", 65)
+
+pastor_aleman.ladrar()
